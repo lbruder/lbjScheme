f73da327c1cde8f95a35dd9ed1fb07077d1fecb6
Leif Bruder <leifbruder@gmail.com>
2013-09-14 22:35:38 +0200

    Added version info

--- src/org/lb/lbjscheme/ConsoleRepl.java
+++ src/org/lb/lbjscheme/ConsoleRepl.java
@@ -28,6 +28,51 @@ public final class ConsoleRepl {
 			System.out.println("-h, -?  Show this text");
 			System.out.println("-i      Use interpreting evaluator (default)");
 			System.out.println("-r      Enter REPL after executing files");
+			System.out.println("-v      Print version info, then quit");
+			System.exit(0);
+		}
+
+		if (hasArgument(args, "-v")) {
+			System.out.println("lbjScheme V0.3");
+			System.out
+					.println("A Scheme subset interpreter in Java, based on SchemeNet.cs");
+			System.out
+					.println("Copyright (c) 2013, Leif Bruder <leifbruder@gmail.com>");
+			System.out
+					.println("Permission to use, copy, modify, and/or distribute this software for any");
+			System.out
+					.println("purpose with or without fee is hereby granted, provided that the above");
+			System.out
+					.println("copyright notice and this permission notice appear in all copies.");
+			System.out.println();
+			System.out
+					.println("THE SOFTWARE IS PROVIDED \"AS IS\" AND THE AUTHOR DISCLAIMS ALL");
+			System.out
+					.println("WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED");
+			System.out
+					.println("WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE");
+			System.out
+					.println("AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR");
+			System.out
+					.println("CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM");
+			System.out
+					.println("LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,");
+			System.out
+					.println("NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN");
+			System.out
+					.println("CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.");
+			System.out.println();
+			System.out
+					.println("BEWARE: This is experimental code. I'm striving for full R5RS");
+			System.out
+					.println("compliance when done, but at the moment there are several parts");
+			System.out
+					.println("missing, such as continuations and multiple value returns. Visit");
+			System.out
+					.println("github.com/lbruder/lbjScheme for more information and updates.");
+			System.out.println("Bug reports are welcome ;)");
+			System.out.println();
+
 			System.exit(0);
 		}
 
